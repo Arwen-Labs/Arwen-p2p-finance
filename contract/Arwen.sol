dc698b6d463affda9c9c9445ee54c216b38684e3
Arwen-Labs <118394519+Arwen-Labs@users.noreply.github.com>
2022-11-27 00:20:03 +0530

    Add files via upload

---
+++ contract/Arwen.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+interface DaiToken {
+    function transfer(address dst, uint wad) external returns (bool);
+    function transferFrom(address src, address dst, uint wad) external returns (bool);
+    function balanceOf(address guy) external view returns (uint);
+}
+
+struct buyerData{
+    uint amount;
+    uint price;
+    address tradeWith;
+    bool conflict;
+}
+struct sellerData{
+    uint amount;
+    uint price;
+    address tradeWith;
+    bool isWithdrawable;
+    bool conflict;
+}
+struct userOrder{
+    bool isOldUser;
+    uint orderId;
+    uint numberOfBuyOrders;
+    uint numberOfSellOrders;
+    uint userFund;
+}
+
+contract Arwen_init {
+
+    mapping (address => buyerData) public buyOrders;
+    mapping (address => sellerData) public sellOrders;
+    mapping (address => userOrder) private order;
+
+    constructor() {
+        //owner = msg.sender; // 'msg.sender' is contract deployer for a constructor
+    }
+
+    function placeSellOrder( uint _amount ,uint _price) public{
+        if( sellOrders[msg.sender].isOldUser == true ){
+            sellerData memory tradeData;
+            tradeData.amount = _amount
+            tradeData.price = _price;
+            tradeData.isWithdrawable = true;
+            tradeData.conflict = false;
+            sellOrders[msg.sender] = tradeData;
+        }else{
+            sellerData memory tradeData;
+            tradeData.amount = _amount;
+            tradeData.isWithdrawable = true;
+            tradeData.conflict = false;
+            sellOrders[msg.sender] = tradeData;
+        }
+    }
+
+    function placeBuyOrder( uint _amount ,uint _price) public{
+        buyerData memory tradeData;
+        tradeData.amount = _amount;
+        tradeData.price = _price;
+        tradeData.conflict = false;
+        buyOrders[msg.sender] = tradeData;
+    }
+
+    function showOrderBalance() public view returns(uint, uint){
+        buyerData memory buyOrderData = buyOrders[msg.sender];
+        sellerData memory sellOrderData = sellOrders[msg.sender];
+        return (buyOrderData.amount, sellOrderData.amount, );
+    }
+}
